bf5ba5c6b0948cb722a40f1a34b70fb16eeab1db
EngineOfDarkness <generalmike1988@hotmail.com>
2021-03-25 18:05:13 +0100

    Added Ability to Configure Changeset and Change as optional

--- gamedata/scripts/config/Changeset.lua
+++ gamedata/scripts/config/Changeset.lua
@@ -3,6 +3,8 @@
     Author: EngineOfDarkness
 
     This work is licensed under the GPL-3.0 License 
+    
+    TODO all this validation methods are too much... perhaps I have to implement a validator object or something like that
 
     --------------------------------------------
 
@@ -24,16 +26,20 @@ local File = require "gamedata\\scripts\\config\\File"
 local Changeset     = {}
 Changeset.__index   = Changeset
 
-local function construct(_, changes, name, ltx)
+local function construct(_, changes, name, ltx, optional)
     local newChangeset = {}
     setmetatable(newChangeset, Changeset)
 
-    newChangeset.changes    = changes
-    newChangeset.name       = name
-    newChangeset.ltx        = ltx
-    newChangeset.errors     = {}
+    newChangeset.changes                = changes
+    newChangeset.invalidChanges         = {} -- will contain invalid optional(!) changes, or if the entire changeset is optional, all failed changes that won't be applied
+    newChangeset.name                   = name
+    newChangeset.ltx                    = ltx
+    newChangeset.optional               = optional
+    newChangeset.errors                 = {} -- is currently used to decide if the changeset is valid, so contains ALL changes that caused the changeset to fail
+    newChangeset.warningsInvalidChanges = {} -- contains the errors of changes that are optional, but not applied due to errors
 
     newChangeset:validateProperties()
+    newChangeset:removeInvalidChanges()
 
     return newChangeset
 end
@@ -61,6 +67,11 @@ function Changeset:validateProperties()
         return
     end
     
+    if type(self.optional) ~= "boolean" and type(self.optional) ~= "nil" then
+        self:addError("Changeset ERROR: 'optional' can only be nil or a boolean value")
+        return
+    end
+    
     if type(self.ltx) ~= "nil" then
         local fileInstance = File(self.ltx)
         
@@ -79,42 +90,96 @@ function Changeset:validateProperties()
 end
 
 function Changeset:validateChanges()
-    for changeIndex, change in pairs(self.changes) do
-        if type(change) ~= "table" or type(change.isValid) ~= "function" then
-            self:addError(string.format("Changeset ERROR: Index '%s' of the given changes is not a Change instance, please see the readme section for 'Change'", changeIndex))
-            goto continue
-        end
-
-        if not change:isValid() then
-            local errorMessages = change.errors
-
-            self:addError(string.format("Changeset ERROR: The Change at index '%s' in this Changeset has errors, see following messages", changeIndex))
-
-            for _, errorMessage in ipairs(errorMessages) do
-                self:addError("> " .. errorMessage)
-            end
-            
-            goto continue
+    for changeIndex, change in ipairs(self.changes) do
+        local isChange = self:isChange(change)
+        local isChangeValid = false
+        local isChangeValidOnLtx = false
+        
+        if isChange then
+            isChangeValid = change:isValid()
         end
         
-        self:validateChangeOnLtx(change)
+        if isChangeValid then
+            isChangeValidOnLtx = self:isChangeValidOnLtx(change)
+        end
 
-        ::continue::
+        self:logChangeErrors(change, changeIndex, isChange, isChangeValid, isChangeValidOnLtx)
     end
 end
 
-function Changeset:validateChangeOnLtx(change)
-    -- either we have a custom ltx to check the section in or the system.ltx
-    local checkIni  = self.ltx and ini_file_ex(self.ltx) or ini_sys
-    local iniName   = self.ltx or "system.ltx"
-
-    if not checkIni:section_exist(change.section) then
-        self:addError(string.format("Change ERROR: the section '%s' in '%s' does not exist, please use Anomalies default methods to add new sections", change.section, iniName))
+-- logging to "logError" means the changeset becomes invalid, logging to logWarning means it won't.
+-- Yeah that's in horrible need of a redo I know.
+function Changeset:logChangeErrors(change, changeIndex, isChange, isChangeValid, isChangeValidOnLtx)
+    if not isChange then
+        self.addError(string.format("Changeset ERROR: Index '%s' of the given changes is not a Change instance, please see the readme section for 'Change'", changeIndex))
+        return
     end
+    
+    local errorLevel = self:getErrorLevel(change)
+    local logFunction = "add"..errorLevel:gsub("^%l", string.upper)
+    
+    if not isChangeValid then
+        self[logFunction](self, string.format("Changeset %s: The Change at index '%s' in this Changeset has errors, see following messages", string.upper(errorLevel), changeIndex))
+
+        for _, errorMessage in ipairs(change.errors) do
+            self[logFunction](self, "> " .. errorMessage)
+        end
+        
+        return
+    end
+    
+    if not isChangeValidOnLtx then
+        local iniName = self.ltx or "system.ltx"
+    
+        self[logFunction](self, string.format("Change %s: the section '%s' in '%s' does not exist, please use Anomalies default methods to add new sections", string.upper(errorLevel), change.section, iniName))
+    end
+end
+
+function Changeset:isChange(change)
+    return type(change) == "table" and type(change.isValid) == "function"
+end
+
+function Changeset:isChangeValidOnLtx(change)
+    -- either we have a custom ltx to check the section in or the system.ltx
+    local checkIni = self.ltx and ini_file_ex(self.ltx) or ini_sys
+
+    return checkIni:section_exist(change.section)
+end
+
+function Changeset:getErrorLevel(change)
+    local errorLevel = "error"
+    
+    if change.optional or (self.optional and type(change.optional) == "nil") then
+        errorLevel = "warning"
+    end
+    
+    return errorLevel
+end
+
+function Changeset:removeInvalidChanges()
+    local validChanges = {}
+    
+    for _, change in ipairs(self.changes) do
+        if self:isChange(change) and change:isValid() and self:isChangeValidOnLtx(change) then
+            validChanges[#validChanges + 1] = change
+        else
+            self.invalidChanges[#self.invalidChanges + 1] = change
+        end
+    end
+    
+    self.changes = validChanges
+end
+
+function Changeset:getChanges()
+    return self.changes
 end
 
 function Changeset:addError(message)
     self.errors[#self.errors + 1] = message
+end
+
+function Changeset:addWarning(message)
+    self.warningsInvalidChanges[#self.warningsInvalidChanges + 1] = message
 end
 
 function Changeset:isValid()
